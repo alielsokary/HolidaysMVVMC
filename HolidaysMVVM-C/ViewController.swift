53d4daea4d219d8467608976704e3daa12fbfd0c
Ali Elsokary <ali.elsokary8@gmail.com>
2021-04-23 07:03:21 +0200

    Initial Commit

---
+++ HolidaysMVVM-C/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HolidaysMVVM-C
+//
+//  Created by Ali Elsokary on 23/04/2021.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
